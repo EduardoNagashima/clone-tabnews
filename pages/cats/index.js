8b7ace752707dd5fcd08429a2acfa65c05c8cff3
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2024-04-08 02:10:45 +0000

    add sakinha endpoint

--- pages/cats/index.js
+++
@@ -1,5 +0,0 @@
-function Cats() {
-  return <img src="https://cataas.com/cat" alt="Random cat" />;
-}
-
-export default Cats;
---
+++ pages/sakinha/index.js
@@ -0,0 +1,24 @@
+function Sakinha() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        color: "white",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "100vw",
+        background: "#4158D0",
+        background:
+          "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
+      }}
+    >
+      <h1>Essa aqui é a Sakinha:</h1>
+      <img
+        src="https://lh3.googleusercontent.com/pw/AP1GczNs0owIfMP0OXwQ3EHu8JNiG9m_WE9K1p7tSXPIAPdVT2KQeU8IBILV7OmOfAImYLo0BcbG9o1XftdOQeMh1HsZN3BcNlrL6ilpD4qEUaqDDlDylv4PD-oRevY9pJNAQ4FxXg1_MY2B_HY6FFumR423JKKJKZYL10p7PSUWzL4eqHEG0XNxLaQ5GfK-cRJbHqbOX8wbGFAr0yc5XM7vekonRiAaS-S9Y3SyJSFCQo5m1EfyCYJt-ID20vYoZd_wgSVRT-UrMhrg_yMvuSiDM59ae5TrzNR7hKfV3dpOHYsT0mk73pRbB8g8ZCU3hlLO_RTg5a6Esp597rY0MTZsa3UVBB1APZowHT6xMlHu1DzHOzpm-P92gw09KYcxT__XxEZM-nWh_hFQvpifS9hOTIMHLXspXqIuBYvD5wUa_uprE8aVf2wKxvZ_EeGiIsbH0AR0FR_0Ok7zgkMUeG5W4ZjRupTHTXSd5wObEJLQEWfZ1t0IcykYyHq5DcfXufZ6yFhsWKbCsGHNZXsd_9lWWVRquUWCr-MSUzs15wKDmWqoaHdfi01RyC4Vtoo9OIvbzSwTXJb1ahXEnEonU4V8jo2WRyGfg3fgdA6tXuQCzU1KDis-DvclegtRqZPGcotC6_E0KLdxpb6_D5k0q6OYIV4ycosK_e49cmKfQAbBC2SPa04jsf81DQjHwMULFN8DifGh95OFy3dJxywb9kdx4UXMrvrpeZkDKwj-pJTkvA5Wwv9MoyFKqYBlHfD-Ohts67zJs4Dbv1vTCg7qOhEUvfbPyli6R11MH--cVaOARwZyC6jOtWcS0BxJteHg3U7NHZR_exk7AFfIdaH6SA3A0tpG5r9N1MgZfEs_EJFo0sGQCu_lN401Fah7F6srVPQOjU9Q6y6dAs9zhJgP7rNDRgRsEGi8JH7iaqUsumUqY-ZZd538rPHIo1cXNSo=w1660-h1245-s-no-gm?authuser=0"
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+}
+
+export default Sakinha;
