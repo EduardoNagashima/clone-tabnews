bcbe5ff50635d324f23bfa570084e4545f194f2f
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2024-05-27 23:14:21 +0000

    finish api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const { server_version } = databaseVersionResult.rows[0];
 
-  console.log(result.rows);
-  response.status(200).json({ key: "Top demais" });
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const { max_connections } = databaseMaxConnectionsResult.rows[0];
+
+  const databaseOpenedConnectionsResult = await database.query({text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;', values: [databaseName]});
+
+  const openedConnections = databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: server_version,
+        max_connections: parseInt(max_connections),
+        opened_connections: openedConnections
+      },
+    },
+  });
 }
 
 export default status;
