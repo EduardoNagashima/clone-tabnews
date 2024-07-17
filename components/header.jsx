8ad31ba62e3d7314b127e35259b9280eb9c7ca05
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2024-07-17 03:39:24 +0000

    add full path of profileDescriptions

--- components/header.jsx
+++ components/header.jsx
@@ -1,4 +1,4 @@
-import { profileDescriptions } from "pages/portifolio/constants";
+import { profileDescriptions } from "pages/portifolio/constants/index.js";
 import { useEffect, useState, useRef } from "react";
 
 export default function Header() {
