2310117779a6715151f11e80048b8d01b5f3595c
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2024-07-31 02:01:33 +0000

    add wait-for-postgres.js script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+    exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+    function handleReturn(error, stdout, stderr) {
+        if (stderr) console.log(stderr);
+
+        if (stdout.search("accepting connections") === -1) {
+            process.stdout.write('.');
+            checkPostgres();
+            return;
+        };
+
+        console.log('\n🟢 Postgres conectado!\n')
+    }
+}
+
+process.stdout.write('\n\n🔴 Aguardando postgres aceitar conexões\n');
+checkPostgres();
