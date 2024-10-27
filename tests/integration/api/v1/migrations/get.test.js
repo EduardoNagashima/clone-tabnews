5358b77538313d612ef6354eb9dccd72a3dbdc85
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2024-10-27 21:15:58 +0000

    refactor: use `orcherstrator.cleanDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,22 +1,19 @@
-import database from "infra/database.js";
-import waitForAllServices from "tests/orchestrator";
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await waitForAllServices();
-  await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
+  await orchestrator.waitForAllServices();
 });
 
-describe("GET /api/v1/migrations",()=>{
-  describe("Anonymous user",()=>{
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
     test("Running pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
-    
+
       const responseBody = await response.json();
-    
+
       expect(response.status).toBe(200);
       expect(Array.isArray(responseBody)).toBe(true);
       expect(responseBody.length).toBeGreaterThan(0);
     });
-  })
-})
-
+  });
+});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,8 @@
-import waitForAllServices from "tests/orchestrator";
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await waitForAllServices();
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
 describe("GET /api/v1/status", () => {
@@ -9,7 +10,9 @@ describe("GET /api/v1/status", () => {
     test("Should get current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
-      const convertedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const convertedUpdatedAt = new Date(
+        responseBody.updated_at,
+      ).toISOString();
 
       expect(response.status).toBe(200);
       expect(responseBody.updated_at).toBeDefined();
@@ -17,6 +20,6 @@ describe("GET /api/v1/status", () => {
       expect(responseBody.dependencies.database.version).toEqual("16.2");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-    })
-  })
+    });
+  });
 });
