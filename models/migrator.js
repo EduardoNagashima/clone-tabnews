aae7bd23c48ca5ca062515259591ae1f20ffdd7d
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2025-03-24 23:58:31 +0000

    feat: creation of migrator model

---
+++ models/migrator.js
@@ -0,0 +1,48 @@
+import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
+import database from "infra/database";
+import { ServiceError } from "infra/errors";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function requestMigrationRunner(options) {
+  const dbClient = await database.getNewClient();
+
+  try {
+    const migration = await migrationRunner({
+      dbClient,
+      ...defaultMigrationOptions,
+      ...options,
+    });
+
+    return migration;
+  } catch (error) {
+    throw new ServiceError({
+      cause: error,
+      message: "Erro ao acessar o banco de dados",
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function listPendingMigrations() {
+  return requestMigrationRunner({ dryRun: true });
+}
+
+async function runMigrations() {
+  return requestMigrationRunner({ dryRun: false });
+}
+
+const migrator = {
+  listPendingMigrations,
+  runMigrations,
+};
+
+export default migrator;
